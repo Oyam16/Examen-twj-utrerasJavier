740835c6c99e8b18bee57be5b452c1e7ca6b8521
Javier <Oyam16>
2017-03-06 22:16:29 -0500

    angular

---
+++ ExamenPokemon/src/app/Services/master-url.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MasterURLService {
+
+  private _Url: string = '';
+  constructor() {
+  // this._Url='https://examen-twj-laracristian-cristian3006.c9users.io/';
+  this._Url='http://localhost:1337/'
+  }
+  getUrl(){
+    return this._Url;
+  }
+
+}
